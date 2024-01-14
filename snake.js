5e1e61751169adba6660367968620d7870de8f4e
Kafa <136751971+Kafa-Kun-K@users.noreply.github.com>
2024-01-15 00:07:30 +0100

    2

--- snake.js
+++ snake.js
@@ -1,104 +1,105 @@
 //board
-var blockSize = 25;
-var rows = 20;
-var cols = 20;
-var board;
-var context; 
+const BoardConsts = {
+    blockSize: 25,
+    rows: 20,
+    cols: 20
+  }
+  var board;
+  var context;
+  var bgimage = new Image();
+  bgimage.src = 'shake.png';
 
-//snake head
-var snakeX = blockSize * 5;
-var snakeY = blockSize * 5;
-
-var velocityX = 0;
-var velocityY = 0;
-
-var snakeBody = [];
-
-//food
-var foodX;
-var foodY;
-
-var gameOver = false;
-
-window.onload = function() {
+  //snake head
+  var player = { snake: { x: BoardConsts.blockSize * 5, y: BoardConsts.blockSize * 5 } }
+  var velocityX = 0;
+  var velocityY = 0;
+  
+  var snakeBody = [];
+  
+  //food
+  var food = {
+    x: 0,
+    y: 0,
+    placeFood: function() {
+      this.x = Math.floor(Math.random() * BoardConsts.cols) * BoardConsts.blockSize;
+      this.y = Math.floor(Math.random() * BoardConsts.rows) * BoardConsts.blockSize;
+    }
+  }
+  
+  var gameOver = false;
+  
+  window.onload = function() {
     board = document.getElementById("board");
-    board.height = rows * blockSize;
-    board.width = cols * blockSize;
+    board.height = BoardConsts.rows * BoardConsts.blockSize;
+    board.width = BoardConsts.cols * BoardConsts.blockSize;
     context = board.getContext("2d"); //used for drawing on the board
-
-    placeFood();
+  
+    food.placeFood();
     document.addEventListener("keyup", changeDirection);
     // update();
-    setInterval(update, 1000/12.5); //100 milliseconds
-}
-
-function update() {
+    setInterval(update, 1000 / 12.5);
+  }
+  
+  function update() {
     if (gameOver) {
-        return;
+      return;
     }
-
-    context.fillStyle="black";
-    context.fillRect(0, 0, board.width, board.height);
-
-    context.fillStyle="red";
-    context.fillRect(foodX, foodY, blockSize, blockSize);
-
-    if (snakeX == foodX && snakeY == foodY) {
-        snakeBody.push([foodX, foodY]);
-        placeFood();
+  
+    context.fillStyle = "black";
+    context.drawImage(bgimage, 0, 0, board.width, board.height);
+    
+    context.fillStyle = "brown";
+    context.fillRect(food.x, food.y, BoardConsts.blockSize, BoardConsts.blockSize);
+  
+    if (player.snake.x == food.x && player.snake.y == food.y) {
+      snakeBody.push([food.x, food.y]);
+      food.placeFood()
     }
-
-    for (let i = snakeBody.length-1; i > 0; i--) {
-        snakeBody[i] = snakeBody[i-1];
+  
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+      snakeBody[i] = snakeBody[i - 1];
     }
     if (snakeBody.length) {
-        snakeBody[0] = [snakeX, snakeY];
+      snakeBody[0] = [player.snake.x, player.snake.y];
     }
-
-    context.fillStyle="lime";
-    snakeX += velocityX * blockSize;
-    snakeY += velocityY * blockSize;
-    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+  
+    context.fillStyle = "darkgreen";
+    player.snake.x += velocityX * BoardConsts.blockSize;
+    player.snake.y += velocityY * BoardConsts.blockSize;
+    context.fillRect(player.snake.x, player.snake.y, BoardConsts.blockSize, BoardConsts.blockSize);
     for (let i = 0; i < snakeBody.length; i++) {
-        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+      context.fillRect(snakeBody[i][0], snakeBody[i][1], BoardConsts.blockSize, BoardConsts.blockSize);
     }
-
+  
     //game over conditions
-    if (snakeX < 0 || snakeX > cols*blockSize || snakeY < 0 || snakeY > rows*blockSize) {
-        gameOver = true;
-        alert("Game Over");
+    if (player.snake.x < 0 || player.snake.x > (BoardConsts.cols * BoardConsts.blockSize) - 1 || player.snake.y < 0 || player.snake.y > (BoardConsts.rows * BoardConsts.blockSize) - 1) {
+      gameOver = true;
+      document.getElementById("sign").innerHTML="Game over";
     }
-
+  
     for (let i = 0; i < snakeBody.length; i++) {
-        if (snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]) {
-            gameOver = true;
-            alert("Game Over");
-        }
+      if (player.snake.x == snakeBody[i][0] && player.snake.y == snakeBody[i][1]) {
+        gameOver = true;
+        document.getElementById("sign").innerHTML="Game over";
+      }
     }
-}
-
-function changeDirection(e) {
+  }
+  
+  function changeDirection(e) {
     if (e.code == "ArrowUp" && velocityY != 1) {
-        velocityX = 0;
-        velocityY = -1;
+      velocityX = 0;
+      velocityY = -1;
     }
     else if (e.code == "ArrowDown" && velocityY != -1) {
-        velocityX = 0;
-        velocityY = 1;
+      velocityX = 0;
+      velocityY = 1;
     }
     else if (e.code == "ArrowLeft" && velocityX != 1) {
-        velocityX = -1;
-        velocityY = 0;
+      velocityX = -1;
+      velocityY = 0;
     }
     else if (e.code == "ArrowRight" && velocityX != -1) {
-        velocityX = 1;
-        velocityY = 0;
+      velocityX = 1;
+      velocityY = 0;
     }
-}
-
-
-function placeFood() {
-    //(0-1) * cols -> (0-19.9999) -> (0-19) * 25
-    foodX = Math.floor(Math.random() * cols) * blockSize;
-    foodY = Math.floor(Math.random() * rows) * blockSize;
-}
+  }
